d3d4d3d6ad9d404a07734830a9a37aa9cff78ffe
Sarah Moore <sfmoore99@gmail.com>
2013-06-26 19:49:30 -0400

    Modified for C4 & TTT

--- tic_tax_client_sinatra.rb
+++ tic_tax_client_sinatra.rb
@@ -1,33 +1,48 @@
 require 'httparty'
 require_relative 'faster_tic_tac_toe_sockets'
+require_relative 'connect_four_sockets'
+require_relative 'jsonobject'
 
-game = TicTacToe::Board.new
-host = "thomasballinger.com"
+puts "Are we playing TicTacToe (T) or Connect Four (C) ?"
+choice = gets.chomp.upcase
+if choice == "T"
+  game = TicTacToe::Board.new
+  suffix = "/TTT"
+else
+  game = ConnectFour::Board.new 
+  suffix = "/C4"
+end
+host = "localhost" #"thomasballinger.com"
 port = 4567
 
 
 puts game.display
 
 
-init_hash = HTTParty.get("http://#{host}:#{port}/play_request").parsed_response #don't know why I'm not getting json
+init_hash = HTTParty.get("http://#{host}:#{port}/play_request#{suffix}").parsed_response #don't know why I'm not getting json
+p init_hash
 if init_hash["board"] 
   player_id = init_hash["player_id"]
   mark = 1
+  puts "my mark is #{mark}"
   game.read_tictax(init_hash["board"])
   game.make_move(game.auto_move(3,mark),mark)
   puts game.display
+  p game.write_tictax
   options = {:body => {:data => game.write_tictax.to_json}}
-  HTTParty.post("http://#{host}:#{port}/submit_board/#{player_id}",options)
+  HTTParty.post("http://#{host}:#{port}/submit_board/#{player_id}#{suffix}",options)
   
 else
   player_id = init_hash["player_id"]
   mark = -1
+  puts "my mark is #{mark}"
 end
+
 
 begin
   begin 
     puts "requesting"
-    response = HTTParty.get("http://#{host}:#{port}/get_board/#{player_id}").parsed_response
+    response = HTTParty.get("http://#{host}:#{port}/get_board/#{player_id}#{suffix}").parsed_response
     #response = JSON.parse(HTTParty.get("http://localhost:5000/get_board/#{player_id}")) ?? not getting JSON
     p response
     sleep 3
@@ -44,6 +59,6 @@ begin
     game.make_move(game.auto_move(3,mark),mark)
     puts game.display
     options = {:body => {:data => game.write_tictax.to_json}}
-    HTTParty.post("http://#{host}:#{port}/submit_board/#{player_id}",options)
+    HTTParty.post("http://#{host}:#{port}/submit_board/#{player_id}#{suffix}",options)
   end
 end until response["winner"]
