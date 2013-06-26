91aab48a17bcf5c8f6d18ca5c7c3b5e890bdb8c0
Sarah Moore <sfmoore99@gmail.com>
2013-06-26 19:50:10 -0400

    Class for json-able 2D arrays

---
+++ jsonable.rb
@@ -0,0 +1,32 @@
+class Jsonable
+
+  def initialize(two_D_array = [])
+    two_D_array.each_with_index do |col, i|
+      instance_variable_set("@col#{i}",col)
+    end
+  end
+
+  def ready_for_json
+    my_hash = {}
+    (0..6).each do |i|
+      my_hash[i] = instance_variable_get("@col#{i}")
+    end
+    my_hash
+  end
+
+  def from_json!(my_hash)
+    my_hash.each do |k,v|
+      instance_variable_set("@col#{k}",v)
+    end
+    self
+  end
+
+  def two_d_array
+    two_d = []
+    (0..6).each_with_index do |i|
+      two_d << instance_variable_get("@col#{i}")
+    end
+    two_d
+  end
+
+end
